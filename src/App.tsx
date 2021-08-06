71016ffb7123016b5f5eb9ca46030afa756dc2e7
Anna Clara <anisferraz@gmail.com>
2021-08-06 18:27:05 -0300

    admin room

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { AuthContextProvider } from './contexts/AuthContext'
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
+import { AdminRoom } from './pages/AdminRoom';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/room/new" component={NewRoom} />
           <Route path="/room/:id" component={Room} />
+          <Route path="/admin/room/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
